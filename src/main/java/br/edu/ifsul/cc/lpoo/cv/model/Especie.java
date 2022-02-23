d1dc81750b3b5346393ce0449e8d13c3bc3c48c1
José Henrique <josehpaludo@gmail.com>
2022-02-23 08:11:20 -0300

    nono commit

--- src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Especie.java
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Especie.java
@@ -27,7 +27,7 @@ public class Especie implements Serializable{
     @GeneratedValue(generator = "seq_especie", strategy = GenerationType.SEQUENCE)
     private Integer id;
     
-    @Column(nullable = false)
+    @Column(nullable = false, length = 80)
     private String nome;
     
     ///Costrutor
