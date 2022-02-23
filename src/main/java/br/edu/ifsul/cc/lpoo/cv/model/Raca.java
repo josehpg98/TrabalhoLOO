8bf208b9c12776be8380fd47ee074a0e1cab9f26
José Henrique <josehpaludo@gmail.com>
2022-02-23 08:16:41 -0300

    décimo primeiro commit

--- src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Raca.java
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Raca.java
@@ -29,7 +29,7 @@ public class Raca implements Serializable{
     @GeneratedValue(generator = "seq_raca", strategy = GenerationType.SEQUENCE)
     private Integer id;
     
-    @Column(nullable = false)
+    @Column(nullable = false, length = 80)
     private String nome;
     
     @ManyToOne///associação
